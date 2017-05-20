3a647bb916295a6530935fe4f1bbd2736f1ced51
Gyula Juháasz <gyu999@gmail.com>
2017-05-20 21:39:15 +0200

    Terrible workaround applied.

--- app/src/main/java/com/gyulajuhasz/example/overridefail/CustomSwitch.java
+++ app/src/main/java/com/gyulajuhasz/example/overridefail/CustomSwitch.java
@@ -5,11 +5,14 @@ import android.content.res.TypedArray;
 import android.graphics.Typeface;
 import android.support.v7.widget.SwitchCompat;
 import android.util.AttributeSet;
+import android.util.Log;
 
 /**
  * Switch that supports using custom fonts.
  */
 public class CustomSwitch extends SwitchCompat {
+
+    private static final String TAG = "CUSTOMSWITCH";
 
     public CustomSwitch(Context context) {
         this(context, null);
@@ -35,6 +38,12 @@ public class CustomSwitch extends SwitchCompat {
     @Override
     public void setTypeface(Typeface typeface) {
         super.setTypeface(typeface);
-        setSwitchTypeface(typeface);
+        try {
+            setSwitchTypeface(typeface);
+            Log.v(TAG, "OK: " + toString(), new Exception("Just for stacktrace"));
+        } catch (final NullPointerException npe) {
+            // Unfortunately, this cannot be prevented because TextView's constructor calls setTypeface :(
+            Log.w(TAG, "NOT OK: " + toString(), npe);
+        }
     }
 }
